d3f84ff337ff2d7363ea6b1a59a87ca68c193dc3
Joe Merten <n4git@jme.de>
2014-05-12 22:12:43 +0200

    Skripttemplate bzgl. Fehlerbehandlung ergänzt

--- Tools/Template.sh
+++ Tools/Template.sh
@@ -5,6 +5,10 @@
 # \project    Multithreaded C++ Framework
 # \file       Template.sh
 # \creation   2014-05-10, Joe Merten
+#-----------------------------------------------------------------------------------------------------------------------
+# Achtung: Wegen bash -e sollte in diesem Skript weder "let" noch "expr" verwendet serden.
+# Statt dessen besser $((...)) verwenden.
+# Siehe auch: http://unix.stackexchange.com/questions/63166/bash-e-exits-when-let-or-expr-evaluates-to-0
 ########################################################################################################################
 
 
@@ -58,6 +62,11 @@ declare   LIGHT="${ESC}[1m"
 
 ########################################################################################################################
 # Falls keine Ansi VT100 Farben gewünscht sind
+#-----------------------------------------------------------------------------------------------------------------------
+# Todo: Folgendes mal genauer angucken: http://stackoverflow.com/questions/64786/error-handling-in-bash
+#     Color the output if it's an interactive terminal
+#    test -t 1 && tput bold; tput setf 4                                 ## red bold
+#    echo -e "\n(!) EXIT HANDLER:\n"
 ########################################################################################################################
 function NoColor {
     BLACK=""; MAROON=""; GREEN=""; OLIVE=""; NAVY=""; PURPLE="" TEAL="" SILVER=""; GRAY=""
@@ -69,34 +78,101 @@ function NoColor {
 
 
 ########################################################################################################################
+#   _____                    _   _
+#  | ____|_  _____ ___ _ __ | |_(_) ___  _ __  ___
+#  |  _| \ \/ / __/ _ \ '_ \| __| |/ _ \| '_ \/ __|
+#  | |___ >  < (_|  __/ |_) | |_| | (_) | | | \__ \
+#  |_____/_/\_\___\___| .__/ \__|_|\___/|_| |_|___/
+#                     |_|
+########################################################################################################################
+
+# siehe auch http://stackoverflow.com/questions/64786/error-handling-in-bash
+# Ohne "errtrace" wird mein OnError() nicht immer gerufen...
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
+########################################################################################################################
 # Terminalfarben restaurieren, wenn Abbruch via Ctrl+C
 ########################################################################################################################
 function OnCtrlC {
     echo "${RED}[*** interrupted ***]${NORMAL}"
+#    # damit bei Ctrl+C auch alle Childprozesse beendet werden
+#    trap SIGINT
+#    kill -INT 0
+#    #exit 1
+}
+trap OnCtrlC SIGINT
+
+
+########################################################################################################################
+# Fehlerbehandlung Hook
+#-----------------------------------------------------------------------------------------------------------------------
+# Da wir das Skript mit "bash -e" ausführen, führt jeder Befehls- oder Funktionsaufruf, der mit !=0 returniert zu einem
+# Skriptabbruch, sofern der entsprechende Exitcode nicht Skriptseitig ausgewertet wird.
+# Siehe auch http://wiki.bash-hackers.org/commands/builtin/set -e
+# Mit dem OnError() stellen wir hier noch mal einen Fuss in die Tür um genau diesen Umstand (unerwartete Skriptbeendigung)
+# sichtbar zu machen.
+########################################################################################################################
+function OnError() {
+    echo "${RED}Script error exception in line $1, exit code $2${NORMAL}" >&2
+
+    # Stacktrace Versuch
+    # http://wiki.bash-hackers.org/commands/builtin/caller
+    # http://stackoverflow.com/questions/685435/bash-stacktrace
+    local i=0;
+    echo -n "${MAROON}" >&2
+    while caller $i >&2; do
+         ((i++))
+    done
+    echo "==="
+    backtrace
+    echo "==="
+    echo -n "${NORMAL}" >&2
+
+    # Kill, um ggf. gestartete Background Childprozesse auch zu beenden
+    trap SIGINT
     kill -INT 0
-    #exit 1
+    exit 2
 }
+trap 'OnError $LINENO $?' ERR
 
 
 ########################################################################################################################
-#   _   _ _ _  __
-#  | | | (_) |/ _| ___
-#  | |_| | | | |_ / _ \
-#  |  _  | | |  _|  __/
-#  |_| |_|_|_|_|  \___|
+# Exit-Hook
+#-----------------------------------------------------------------------------------------------------------------------
+# OnExit() wird bei jeder Art der Skriptbeendigung aufgerufen, ggf. nach OnError()
+# Siehe auch http://wiki.bash-hackers.org/commands/builtin/trap
+#
+# TODO: Näher untersuchen und für mich anpassen
+#   tempfiles=( )
+#   cleanup() {
+#       rm -f "${tempfiles[@]}"
+#   }
+#   trap cleanup EXIT
 ########################################################################################################################
+function OnExit() {
+    if [ "$2" != "0" ]; then
+        echo "${RED}Script exitcode=$2${NORMAL}" >&2
+    else
+        : # echo "${TEAL}Script exitcode=$2${NORMAL}" >&2
+    fi
 
-########################################################################################################################
-# Hilfe
-########################################################################################################################
-function ShowHelp {
-    echo "${AQUA}Shellskript Template${TEAL}, Joe Merten 2014"
-    echo "usage: $0 [options] ..."
-    echo "Available options:"
-    echo "  nocolor       - Dont use Ansi VT100 colors"
-    echo "  -r            - Act on subdirectories"
-    echo -n ${NORMAL}
+    # Ggf. Childprozesse beenden
+    #trap SIGINT
+    #kill -INT 0
+
+    # Ggf. Terminalfarben restaurieren
+    echo -n "${NORMAL}"
 }
+trap 'OnExit $LINENO $?' EXIT
+
+
+# Zum Einbinden des Codes aus http://stackoverflow.com/questions/64786/error-handling-in-bash
+# Weil der hat einen besseren Stacktrace.
+# Allerding hebelt der meine eigenen obigen Hooks aus.
+#[ -f /usr/local/bin/lib.trap.sh ] && set +o nounset && source /usr/local/bin/lib.trap.sh
 
 
 ########################################################################################################################
@@ -121,6 +197,7 @@ declare LOG_COLOR_TRACE="${BLUE}"
 
 function Fatal {
     echo "${LOG_COLOR_FATAL}*** Fatal: $*${NORMAL}" >&2
+    trap SIGINT
     kill -INT 0
     echo "+++++++++++++++++++++++++++++++"
 }
@@ -144,6 +221,34 @@ function Debug {
 
 function Trace {
     echo "${LOG_COLOR_TRACE}Trace: $*${NORMAL}" >&2
+}
+
+
+########################################################################################################################
+#   _____         _   _
+#  |_   _|__  ___| |_(_)_ __   __ _
+#    | |/ _ \/ __| __| | '_ \ / _` |
+#    | |  __/\__ \ |_| | | | | (_| |
+#    |_|\___||___/\__|_|_| |_|\__, |
+#                             |___/
+#-----------------------------------------------------------------------------------------------------------------------
+# Ein Miniatur Testframework für Shellskripte
+########################################################################################################################
+
+########################################################################################################################
+# Stringvergleich
+#-----------------------------------------------------------------------------------------------------------------------
+# \in  actual    Erhaltener Wert
+# \in  expected  Erwarteter Wert
+#-----------------------------------------------------------------------------------------------------------------------
+# Vergleiche die beiden übergebenen Strings und gibt bei Ungleichheit eine Fehlermeldung aus
+########################################################################################################################
+function Test_Check {
+    local actual="$1"
+    local expected="$2"
+    if [ "$actual" != "$expected" ]; then
+        Error "Test failed. Expected \"$expected\" but got \"$actual\" from $(caller)."
+    fi
 }
 
 
@@ -194,6 +299,75 @@ function WithDots {
 
 
 ########################################################################################################################
+# Fileext ermitteln
+########################################################################################################################
+function GetFileExt {
+    local filename="$1"
+    local base="$(basename "$filename")"
+
+    # Vorabprüfung: Eine Fileext ist nur enthalten, wenn der String einen Punkt enthält, vor diesem aber eine "nicht-Punkt" ist
+    if ! [[ "$base" =~ [^.]\. ]]; then
+        echo ""
+        return
+    fi
+
+    # Prüfung, ob überhaupt ein Punkt enthalten ist
+    #if [ "${base%.*}" == "$base" ]; then
+    #    # Kein . enthalten → also auch keine Fileext
+    #    echo ""
+    #    return
+    #fi
+
+    #echo "${base%%.*}"  #  example.a.b.c.d  →  example
+    #echo "${base%.*}"   #  example.a.b.c.d  →  example.a.b.c
+    #echo "${base#*.}"   #  example.a.b.c.d  →  a.b.c.d
+    echo "${base##*.}"  #  example.a.b.c.d  →  d
+}
+
+function Test_GetFileExt {
+    Test_Check "$(GetFileExt "verzeichnis  /example.a")"       "a"
+    Test_Check "$(GetFileExt "verzeichnis  /example.a.b.c.d")" "d"
+    Test_Check "$(GetFileExt "verzeichnis  /example.")"        ""
+    Test_Check "$(GetFileExt "verzeichnis  /example")"         ""
+
+    Test_Check "$(GetFileExt "")"              ""
+    Test_Check "$(GetFileExt ".")"             ""
+    Test_Check "$(GetFileExt "..")"            ""
+    Test_Check "$(GetFileExt "...")"           ""
+    Test_Check "$(GetFileExt "/x.y.z./...")"   ""
+    Test_Check "$(GetFileExt "/x.y.z./...a")"  ""
+
+    Test_Check "$(GetFileExt "Basename.ext with space")"  "ext with space"
+
+    Test_Check "$(GetFileExt ".project.bla")"  "bla"
+    Test_Check "$(GetFileExt ".project")"      ""
+    Test_Check "$(GetFileExt " .project")"     "project"
+}
+#Test_GetFileExt
+
+
+########################################################################################################################
+#   _   _ _ _  __
+#  | | | (_) |/ _| ___
+#  | |_| | | | |_ / _ \
+#  |  _  | | |  _|  __/
+#  |_| |_|_|_|_|  \___|
+########################################################################################################################
+
+########################################################################################################################
+# Hilfe
+########################################################################################################################
+function ShowHelp {
+    echo "${AQUA}Shellskript Template${TEAL}, Joe Merten 2014"
+    echo "usage: $0 [options] ..."
+    echo "Available options:"
+    echo "  nocolor       - Dont use Ansi VT100 colors"
+    echo "  -r            - Act on subdirectories"
+    echo -n ${NORMAL}
+}
+
+
+########################################################################################################################
 #   ____                                _
 #  |  _ \ __ _ _ __ __ _ _ __ ___   ___| |_ ___ _ __
 #  | |_) / _` | '__/ _` | '_ ` _ \ / _ \ __/ _ \ '__|
@@ -238,4 +412,18 @@ Debug   "Debugausgabe"
 Info    "Infoausgabe"
 Warning "Warnmeldung"
 Error   "Fehlermeldung"
+
+function func3() {
+    ls $1
+}
+function func2() {
+    func3 $1
+}
+function func1() {
+    func2 $1
+}
+#ls asdfsadf
+func1 tralala
+
 Fatal   "Fatal Fehler"
+echo "Ende"
