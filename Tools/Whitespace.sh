576f6e8247daa4046ec02472a6abf36b402d1668
Joe Merten <n4git@jme.de>
2014-05-16 12:06:51 +0200

    Whitespaceskript: Überarbeitung, u.a. Dirs jetzt im Bash Array

--- Tools/Whitespace.sh
+++ Tools/Whitespace.sh
@@ -6,8 +6,9 @@
 # \file       Whitespace.sh
 # \creation   2014-05-10, Joe Merten
 #-----------------------------------------------------------------------------------------------------------------------
-# Achtung: Wegen bash -e sollte in diesem Skript weder "let" noch "expr" verwendet serden.
-# Statt dessen besser $((...)) verwenden.
+# Achtung: Wegen bash -e sollte in diesem Skript weder "let" noch "expr" verwendet serden. ((i++)) ist ebenfalls problematisch.
+# Workaround: "||:" dahinter schreiben, also z.B.:
+#   let 'i++' ||:
 # Siehe auch: http://unix.stackexchange.com/questions/63166/bash-e-exits-when-let-or-expr-evaluates-to-0
 ########################################################################################################################
 
@@ -24,8 +25,8 @@
 # Konstanten & Globale Variablen
 ########################################################################################################################
 
-declare DIRS="."
-declare FILE_PATTERNS=""
+declare DIRS=()
+declare FILE_PATTERNS=()
 declare MODIFY=""
 
 ########################################################################################################################
@@ -95,14 +96,30 @@ set -o nounset   ## set -u : exit the script if you try to use an uninitialised 
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
 ########################################################################################################################
+# Terminalfarben restaurieren, ggf. Childprozesse beenden et cetera
+########################################################################################################################
+declare SHUTTING_DOWN=
+function StopScript {
+    SHUTTING_DOWN="true"
+    local exitcode="$1"
+
+    echo -n "${NORMAL}"
+    echo -n "${NORMAL}" >&2
+
+    # Kill, um ggf. gestartete Background Childprozesse auch zu beenden
+    trap SIGINT
+    kill -INT 0
+    exit $exitcode
+}
+
+########################################################################################################################
 # Terminalfarben restaurieren, wenn Abbruch via Ctrl+C
 ########################################################################################################################
 function OnCtrlC {
-    echo "${RED}[*** interrupted ***]${NORMAL}"
-#    # damit bei Ctrl+C auch alle Childprozesse beendet werden
-#    trap SIGINT
-#    kill -INT 0
-#    #exit 1
+    [ "$SHUTTING_DOWN" != "" ] && return 0
+    echo "${RED}[*** interrupted ***]${NORMAL}" >&2
+    # damit bei Ctrl+C auch alle Childprozesse beendet werden etc.
+    StopScript 2
 }
 trap OnCtrlC SIGINT
 
@@ -119,23 +136,17 @@ trap OnCtrlC SIGINT
 function OnError() {
     echo "${RED}Script error exception in line $1, exit code $2${NORMAL}" >&2
 
-    # Stacktrace Versuch
+    # Stacktrace ausgeben
     # http://wiki.bash-hackers.org/commands/builtin/caller
     # http://stackoverflow.com/questions/685435/bash-stacktrace
     local i=0;
+    local s=""
     echo -n "${MAROON}" >&2
-    while caller $i >&2; do
-         ((i++))
+    while s="$(caller $i)"; do
+        echo "  ${MAROON}$s${NORMAL}" >&2
+        ((i++)) ||:
     done
-    echo "==="
-    backtrace
-    echo "==="
-    echo -n "${NORMAL}" >&2
-
-    # Kill, um ggf. gestartete Background Childprozesse auch zu beenden
-    trap SIGINT
-    kill -INT 0
-    exit 2
+    StopScript 2
 }
 trap 'OnError $LINENO $?' ERR
 
@@ -154,26 +165,16 @@ trap 'OnError $LINENO $?' ERR
 #   trap cleanup EXIT
 ########################################################################################################################
 function OnExit() {
-    if [ "$2" != "0" ]; then
-        echo "${RED}Script exitcode=$2${NORMAL}" >&2
+    local exitcode="$1"
+        if [ "$2" != "0" ]; then
+        echo "${RED}Script exitcode=$exitcode${NORMAL}" >&2
     else
-        : # echo "${TEAL}Script exitcode=$2${NORMAL}" >&2
+        : # echo "${TEAL}Script exitcode=$exitcode${NORMAL}" >&2
     fi
-
-    # Ggf. Childprozesse beenden
-    #trap SIGINT
-    #kill -INT 0
-
-    # Ggf. Terminalfarben restaurieren
-    echo -n "${NORMAL}"
+    # TODO: Hier wirklich noch mal exit aufrufen?
+    StopScript $exitcode
 }
 trap 'OnExit $LINENO $?' EXIT
-
-
-# Zum Einbinden des Codes aus http://stackoverflow.com/questions/64786/error-handling-in-bash
-# Weil der hat einen besseren Stacktrace.
-# Allerding hebelt der meine eigenen obigen Hooks aus.
-#[ -f /usr/local/bin/lib.trap.sh ] && set +o nounset && source /usr/local/bin/lib.trap.sh
 
 
 ########################################################################################################################
@@ -198,8 +199,7 @@ declare LOG_COLOR_TRACE="${BLUE}"
 
 function Fatal {
     echo "${LOG_COLOR_FATAL}*** Fatal: $*${NORMAL}" >&2
-    trap SIGINT
-    kill -INT 0
+    StopScript 2
     echo "+++++++++++++++++++++++++++++++"
 }
 
@@ -280,7 +280,7 @@ function WithDots {
 
     # Dots einfügen
     while [ "$IDX" -gt "3" ]; do
-        let "IDX -= 3"
+        let "IDX -= 3" ||:
         L=${RET:0:$IDX}
         R=${RET:$IDX}
         RET="$L.$R"
@@ -300,32 +300,49 @@ function WithDots {
 
 
 ########################################################################################################################
-# Fileext ermitteln
+# Ermittlung von File Base oder Ext
+#-----------------------------------------------------------------------------------------------------------------------
+# \in  filename  Kompletter Dateiname, optional mit Verzeichnis
+# \in  mode      - "base" = File Base wird ermittelt
+#                - "ext"  = File Ext wird ermittelt
+#-----------------------------------------------------------------------------------------------------------------------
+
 ########################################################################################################################
-function GetFileExt {
+function GetFileBaseExt {
     local filename="$1"
-    local base="$(basename "$filename")"
+    local mode="$2"
+    local name="$(basename "$filename")"
 
     # Vorabprüfung: Eine Fileext ist nur enthalten, wenn der String einen Punkt enthält, vor diesem aber eine "nicht-Punkt" ist
-    if ! [[ "$base" =~ [^.]\. ]]; then
-        echo ""
-        return
+    if ! [[ "$name" =~ [^.]\. ]]; then
+        [ "$mode" == "base" ] && echo "$name"
+        [ "$mode" == "ext" ] && echo ""
+        return 0
     fi
 
     # Prüfung, ob überhaupt ein Punkt enthalten ist
-    #if [ "${base%.*}" == "$base" ]; then
+    #if [ "${name%.*}" == "$name" ]; then
     #    # Kein . enthalten → also auch keine Fileext
     #    echo ""
     #    return
     #fi
 
-    #echo "${base%%.*}"  #  example.a.b.c.d  →  example
-    #echo "${base%.*}"   #  example.a.b.c.d  →  example.a.b.c
-    #echo "${base#*.}"   #  example.a.b.c.d  →  a.b.c.d
-    echo "${base##*.}"  #  example.a.b.c.d  →  d
+                            #echo "${name%%.*}"  #  example.a.b.c.d  →  example
+    [ "$mode" == "base" ] && echo "${name%.*}"   #  example.a.b.c.d  →  example.a.b.c
+                            #echo "${name#*.}"   #  example.a.b.c.d  →  a.b.c.d
+    [ "$mode" == "ext" ]  && echo "${name##*.}"  #  example.a.b.c.d  →  d
+    return 0
 }
 
-function Test_GetFileExt {
+function GetFileBase {
+    GetFileBaseExt "$1" base
+}
+
+function GetFileExt {
+    GetFileBaseExt "$1" ext
+}
+
+function Test_GetFileBaseExt {
     Test_Check "$(GetFileExt "verzeichnis  /example.a")"       "a"
     Test_Check "$(GetFileExt "verzeichnis  /example.a.b.c.d")" "d"
     Test_Check "$(GetFileExt "verzeichnis  /example.")"        ""
@@ -343,8 +360,27 @@ function Test_GetFileExt {
     Test_Check "$(GetFileExt ".project.bla")"  "bla"
     Test_Check "$(GetFileExt ".project")"      ""
     Test_Check "$(GetFileExt " .project")"     "project"
+
+
+    Test_Check "$(GetFileBase "verzeichnis  /example.a")"       "example"
+    Test_Check "$(GetFileBase "verzeichnis  /example.a.b.c.d")" "example.a.b.c"
+    Test_Check "$(GetFileBase "verzeichnis  /example.")"        "example"
+    Test_Check "$(GetFileBase "verzeichnis  /example")"         "example"
+
+    Test_Check "$(GetFileBase "")"              ""
+    Test_Check "$(GetFileBase ".")"             "."
+    Test_Check "$(GetFileBase "..")"            ".."
+    Test_Check "$(GetFileBase "...")"           "..."
+    Test_Check "$(GetFileBase "/x.y.z./...")"   "..."
+    Test_Check "$(GetFileBase "/x.y.z./...a")"  "...a"
+
+    Test_Check "$(GetFileBase "Basename.ext with space")"  "Basename"
+
+    Test_Check "$(GetFileBase ".project.bla")"  ".project"
+    Test_Check "$(GetFileBase ".project")"      ".project"
+    Test_Check "$(GetFileBase " .project")"     " "
 }
-#Test_GetFileExt
+#Test_GetFileBaseExt
 
 
 ########################################################################################################################
@@ -386,8 +422,9 @@ function CheckLineend {
 }
 
 function CheckTabs {
+    #blubber="$(ls blubber)" # Testcode um das Skriptverhalten bei Fehlern zu testen
     local filename="$1"
-    local base="$(basename "$filename")"
+    local base="$(GetFileBase "$filename")"
     local ext="$(GetFileExt "$filename")"
 
     if [ "$base" == "Makefile" ] || [ "$ext" == "mk" ] || [ "$ext" == "mak" ]; then
@@ -399,6 +436,7 @@ function CheckTabs {
     fi
     if [ "$MODIFY" == "" ]; then
         Warning "$filename contains tabs"
+        #Trace "Base=\"$base\" Ext=\"$ext\""
         return
     fi
 
@@ -438,9 +476,9 @@ function CheckTrailingWhitespace {
 
 function CheckAnsiCodes {
     local filename="$1"
-    local apo=$'\x92'
-    local gans1=$'\x93'
-    local gans2=$'\x94'
+    #local apo=$'\x92'
+    #local gans1=$'\x93'
+    #local gans2=$'\x94'
     if [ "$MODIFY" == "" ]; then
         Warning "$filename has ansi codes"
         return
@@ -466,19 +504,24 @@ function CheckAnsiCodes {
 ########################################################################################################################
 function DoFile {
     local filename="$1"
-    local base="$(basename "$filename")"
+    local name="$(basename "$filename")"
     local ext="$(GetFileExt "$filename")"
     #Trace "Preparing ${filename}"
     #if [ "$ext" != "" ]; then
     #    Trace "Fileext = $ext"
     #else
-    #    Trace "Base = $base"
+    #    Trace "Name = $name"
     #fi
 
     # Hier etwas Sicherheitsprüfung, damit ich nicht mal versehentlich eine Binärdatei erwische
     local type="$(file --brief "$filename")"
     #Trace "$filename: $type"
     case "$type" in
+        "empty")
+            # Leere Dateien ignorieren wir hier
+            # gefunden in /D/git/N4/Embedded/Thrift/thrift-0.9.1/lib/rb/ext/protocol.h
+            return 0;;
+
         "ASCII text");;
         "ASCII text, with CRLF line terminators");;
         "ASCII English text");;
@@ -489,15 +532,21 @@ function DoFile {
         "UTF-8 Unicode English text, with CRLF line terminators");;
 
         "ASCII English text, with CRLF, LF line terminators");;
+        "ASCII C++ program text, with CRLF, LF line terminators");;
 
         "Python script, ASCII text executable");;  # Hmm, komischerweise wird "Lib/Stm/Stm32F10x_StdPeriph_Lib_V3.5.0/Utilities/STM32_EVAL/Common/fonts.c" als solchiges erkannt
+        "Bourne-Again shell script, UTF-8 Unicode text executable");;
 
         "UTF-8 Unicode C program text");;
         "UTF-8 Unicode C program text, with CRLF line terminators");;
+        "UTF-8 Unicode C++ program text");;
         "ASCII C program text");;
         "ASCII C program text, with CRLF line terminators");;
         "ASCII C++ program text");;
         "ASCII C++ program text, with CRLF line terminators");;
+        "ASCII make commands text");; # Makefile
+
+        "C program text (from flex), ");; # Thrift/thrift-0.9.1/compiler/cpp/thriftl.cc
 
         "HTML document");;
         "HTML document, ASCII text");;
@@ -516,7 +565,7 @@ function DoFile {
 
         "HTML document, Non-ISO extended-ASCII text, with CRLF line terminators")
             Warning "$filename has type \"$type\""
-            [ "$base" == "readme.txt" ] && CheckAnsiCodes "$filename" # Komischerweise werden einige readme.txt als html eingestuft, wohl wegen der enthaltenen "<a href=...>"
+            [ "$name" == "readme.txt" ] && CheckAnsiCodes "$filename" # Komischerweise werden einige readme.txt als html eingestuft, wohl wegen der enthaltenen "<a href=...>"
             return 0;;
 
         "ISO-8859 English text, with CRLF line terminators");&
@@ -544,13 +593,22 @@ function DoFile {
 function DoAllFiles {
     Trace "Collecting Files"
 
+    # Umwandlung der FILE_PATTERNS in einen für find passenden Regular Expression
+    local re=""
+    for pat in "${FILE_PATTERNS[@]}"; do
+      [ "$re" != "" ] && re+='|'
+      re+="$pat"
+    done
+    re="($re)"
+    #Trace "Find RE = \"$re\""
+
     # Etwas Aufwand um auch mit Leerzeichen in Dateinamen umgehen zu können
     local files=()
     local i="0"
     # Erst mal alle Files einsammeln
     while IFS= read -r -d $'\0' file; do
         files[i++]="$file"
-    done < <(find "${DIRS}" -type f -regextype posix-egrep -regex "${FILE_PATTERNS}" -print0)
+    done < <(find "${DIRS[@]}" -type f -regextype posix-egrep -regex "${re}" -print0)
 
     Trace "Found ${#files[@]} Files"
 
@@ -559,6 +617,21 @@ function DoAllFiles {
     done
 }
 
+
+########################################################################################################################
+# Auflistung aller Dateitypen
+########################################################################################################################
+function DoListFileTypes {
+    local ext=""
+    while IFS= read -r -d $'\0' file; do
+        ext="$(GetFileExt "$file")"
+        if [ "$ext" != "" ]; then
+            echo "*.$ext"
+        else
+            echo "$(basename "$file")"
+        fi
+    done < <(find "${DIRS[@]}" -type f -print0) | LC_ALL=C sort -u
+}
 
 ########################################################################################################################
 #   _   _ _ _  __
@@ -601,9 +674,10 @@ while (("$#")); do
   elif [ "$1" == "-m" ]; then
     MODIFY="true"
   else
-    echo "Unexpected parameter \"$1\"" >&2
-    ShowHelp
-    exit 1
+    DIRS+=("$1")
+    #echo "Unexpected parameter \"$1\"" >&2
+    #ShowHelp
+    #exit 1
   fi
   shift
 done
@@ -634,23 +708,33 @@ done
 # - Lib/Stm/Stm32F2xx_StdPeriph_Lib_V1.1.0/Project/STM32F2xx_StdPeriph_Examples/PWR/STOP/readme.txt
 #   -> enthält Ansicode ™ in "Cortex™-M3"
 
+# Wenn kein Verzeichnis angegeben, dann defaulten wir auf "."
+[ "${#DIRS[@]}" == "0" ] && DIRS+=('.')
 
-DIRS="Lib"
-#DIRS="Lib/Stm/Stm32F10x_StdPeriph_Lib_V3.5.0/Libraries"
-FILE_PATTERNS="("
-FILE_PATTERNS="${FILE_PATTERNS}.*\.h|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.c|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.hxx|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.cxx|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.s|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.S|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.ld|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.lds|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.txt|"
-FILE_PATTERNS="${FILE_PATTERNS}.*\.md|"
-FILE_PATTERNS="${FILE_PATTERNS}DUMMY_EINTRAG)"
-#FILE_PATTERNS=".*"
+#DoListFileTypes
 
+# Meine FilePatterns sind Regular Expressions (wg. find)
+FILE_PATTERNS+=('.*\.h')
+FILE_PATTERNS+=('.*\.c')
+FILE_PATTERNS+=('.*\.hxx')
+FILE_PATTERNS+=('.*\.cxx')
+FILE_PATTERNS+=('.*\.hpp')
+FILE_PATTERNS+=('.*\.cpp')
+FILE_PATTERNS+=('.*\.cc')  # Wg. Thrift
+FILE_PATTERNS+=('.*\.s')
+FILE_PATTERNS+=('.*\.S')
+FILE_PATTERNS+=('.*\.ld')
+FILE_PATTERNS+=('.*\.lds')
+FILE_PATTERNS+=('.*\.txt')
+FILE_PATTERNS+=('.*\.md')
+FILE_PATTERNS+=('.*Makefile')
+FILE_PATTERNS+=('.*Makefile\..*') # z.B. für "Makefile.posix"
+FILE_PATTERNS+=('.*\.mk')
+FILE_PATTERNS+=('.*\.sh')
+FILE_PATTERNS+=('.*\.bsh')
+
+#DIRS=("Lib/Stm/Stm32F10x_StdPeriph_Lib_V3.5.0/Libraries")
+#FILE_PATTERNS=(".*")
 #FILE_PATTERNS="(Lib/Stm/Stm32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/startup/gcc_ride7/startup_stm32f10x_ld.s|.*\.md)"
 #FILE_PATTERNS="(.*/startup_stm32f10x_ld.s|.*\.md)"
 #FILE_PATTERNS="(Lib/Stm/Stm32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_rcc.c)"
