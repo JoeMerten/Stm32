5605ba5985973804467c451800e2d6636cf48389
Joe Merten <git@jme.de>
2017-03-05 18:47:00 +0100

    AnsiColors.py: more tests for [7m and updated test results

--- Tools/AnsiColors.py
+++ Tools/AnsiColors.py
@@ -22,6 +22,8 @@
 # ├─────────────────┼──────┼───────┼────────┼────────┼───────┼─────────┼─────────┼────────┼────────┼─────────┼─────────┼─────┼──────┼───────┼────────┼────────┼───────┤
 # │ macOS console   │  ok  │  ok   │   -    │   ok   │ slow  │   ok    │   ok    │   -    │   -    │   ok    │   ok    │  -  │  -   │   -   │   -    │   -    │  ok   │
 # ├─────────────────┼──────┼───────┼────────┼────────┼───────┼─────────┼─────────┼────────┼────────┼─────────┼─────────┼─────┼──────┼───────┼────────┼────────┼───────┤
+# │ Kubuntu xterm   │  ok  │  ok   │   ok   │   ok   │ slow  │   ok    │   ok    │ single │   ok   │   ok    │   ok    │ ok* │  -   │   -   │   -    │   -    │  ok   │
+# ├─────────────────┼──────┼───────┼────────┼────────┼───────┼─────────┼─────────┼────────┼────────┼─────────┼─────────┼─────┼──────┼───────┼────────┼────────┼───────┤
 # │ Win Teraterm    │  ok  │   -   │   -    │   ok   │   -   │   ok    │    -    │   -    │   -    │   ok    │   ok    │ ok  │  -   │   -   │   -    │   -    │  ok   │
 # ├─────────────────┼──────┼───────┼────────┼────────┼───────┼─────────┼─────────┼────────┼────────┼─────────┼─────────┼─────┼──────┼───────┼────────┼────────┼───────┤
 # │ Eclipse         │  ok  │   -   │   ok   │   ok   │   -   │   ok    │   ok    │   ok   │   ok   │   ok    │   ok    │ ok  │  -   │  ok   │   -    │   -    │  ok   │
@@ -35,6 +37,10 @@
 #   • blink slow [5m works, but blink fast [6m not
 # ⚫ macOS console
 #   • reset single attributes don't works with foreground [39m and background color [49m
+# ⚫ xterm testet version 322 in Kubuntu 16.04
+#   • xterm stops blinking when window loose focus
+#   • double underline just underlines single (might depend on font size?)
+#   • rgb colors were parsed, but displayed incorrect; e.g. dark magenta became gray and even no smooth gradient rendering
 # ⚫ Win Teraterm 4.9
 #   • [1m changes foreground color to yellow (if default color)
 #   • [5m changes foreground color to red (if default color) and also brighten background color (if not default background color)
@@ -48,12 +54,14 @@
 #   • version 0.4.3
 #   • conceal suppresses output completely, there is no placeholder (e.g. whitespace) output
 #   • reset single attributes don't works with conceal
+#   • version 0.4.4 will got much more ansi attribute and color functionality (work in progress)
 # ⚫ Mac XcodeColors
 #   • Still no Ansi support, see https://github.com/robbiehanson/XcodeColors/issues/66
 # ⚫ My mission
 #   • Jenkins AnsiColor: add italic, inverse, strike, color90, palette, rgb - and maybe fix conceal issues
 #     - homepage: https://wiki.jenkins-ci.org/display/JENKINS/AnsiColor+Plugin
 #     - My issue report: https://github.com/dblock/jenkins-ansicolor-plugin/issues/89
+#     - For inverse: http://stackoverflow.com/questions/42585913/how-to-display-inverse-text-in-html
 ########################################################################################################################
 
 
@@ -170,6 +178,7 @@ def resetAttributes():
 
     for s in range(1, len(styles)):
         print("{:10}: ".format(styles[s]["name"]), end='')
+        print("\x1B[{}mon\x1B[{}moff\x1B[m  ".format(styles[s]["on"], styles[s]["off"]), end='')
         print("\x1B[33;41m\x1B[{}mon\x1B[{}moff\x1B[m".format(styles[s]["on"], styles[s]["off"]), end='')
         print("")
 
@@ -188,6 +197,57 @@ def frames():
 
 
 ########################################################################################################################
+# As read in https://en.wikipedia.org/wiki/ANSI_escape_code, the basic colors might be a bit different from the 16 first
+# entries of the 256 color palette.
+########################################################################################################################
+def standardVersusPaletteColors():
+    print("┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────┐")
+    print("│ Comparing standard colors esc[40m, esc[100m, ... versus first 16 palette colors esc[48;5;0m ... esc[48;5;15m │")
+    print("└──────────────────────────────────────────────────────────────────────────────────────────────────────────────┘")
+
+    print("Standard: ", end='')
+    for i in range(40, 48): print("\x1B[{}m {:3d} \x1B[m".format(i, i), end='')
+    for i in range(100, 108): print("\x1B[{}m {:3d} \x1B[m".format(i, i), end='')
+    print("")
+    print("Palette:  ", end='')
+    for i in range(0, 16): print("\x1B[48;5;{}m {:3d} \x1B[m".format(i, i), end='')
+    print("")
+
+
+########################################################################################################################
+# Combinations with inverse attribute
+########################################################################################################################
+def inverseCombinations():
+    print("┌───────────────────────────────┐")
+    print("│ Some combinations with esc[7m │")
+    print("└───────────────────────────────┘")
+
+    print("\x1B[33;41m" + "yellow on red, " +
+          "\x1B[7m" + "now inverse, " +
+          "\x1B[7m" + "one more [7m should change nothing, " +
+          "\x1B[27m" + "turned back to non inverse" + "\x1B[m")
+
+    print("\x1B[33;41m" + "yellow on red, " +
+          "\x1B[7m" + "now inverse, " +
+          "\x1B[30m" + "[30m → red on black, " +
+          "\x1B[103m" + "[103m  → yellow on black, " +
+          "\x1B[27m" + "[27m → black on yellow" + "\x1B[m")
+
+
+########################################################################################################################
+# Hmm won't work this way?
+########################################################################################################################
+def xtermDefaultColors():
+    print("┌──────────────────────┐")
+    print("│ xterm default colors │")
+    print("└──────────────────────┘")
+
+    print("\x1B]10;1\x07" + "foreground 31")
+    print("\x1B]10;3\x07" + "background 33")
+    print("\x1B[mafter[m")
+
+
+########################################################################################################################
 #   __  __       _
 #  |  \/  | __ _(_)_ __
 #  | |\/| |/ _` | | '_ \
@@ -201,6 +261,9 @@ def main():
    font()
    resetAttributes()
    frames()
+   standardVersusPaletteColors()
+   inverseCombinations()
+   #xtermDefaultColors()
 
 
 ########################################################################################################################
