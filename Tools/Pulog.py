d398f3672189fb6f45c8af6058927584390b2472
Joe Merten <github@jme.de>
2017-03-18 13:11:42 +0100

    Renamed github username to JoeMerten without dash

--- Tools/Pulog.py
+++ Tools/Pulog.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 if __name__ == '__main__':
-    print("Project has been migrated to https://github.com/Joe-Merten/Jfe/tree/master/Framework/tools/Pulog.py")
+    print("Project has been migrated to https://github.com/JoeMerten/Jfe/tree/master/Framework/tools/Pulog.py")
